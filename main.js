422744275fd73dbca4216e47711a4757db5a5d33
TayyabAziz11 <159780590+TayyabAziz11@users.noreply.github.com>
2024-03-13 13:13:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters
+var personname = "\n\t Tayyab Aziz \t\n";
+console.log(personname);
+var stripped = personname.trim();
+console.log(stripped);
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters
+
+
+let personname: string= "\n\t Tayyab Aziz \t\n"
+
+console.log(personname);
+
+let stripped :string=personname.trim();
+
+console.log(stripped);
